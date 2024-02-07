d2a13cc0e60c3fb073333133918d3492c4ee398f
Admin <funedy@yandex.ru>
2024-02-07 12:29:09 +0300

    1.2

--- src/main/java/by/teachmeskills/Main.java
+++ src/main/java/by/teachmeskills/Main.java
@@ -6,7 +6,7 @@ import by.teachmeskills.repository.ShopRepository;
 
 public class Main {
     public static void main(String[] args) {
-        FileRepository fileRepository = new FileRepository();
+        ShopRepository fileRepository = new FileRepository();
         User user = new User(1L,"Yara","Smit", "Pupa", "1998");
         User user1 = new User(2L,"Yara","Smit", "Pupa", "1998");
         fileRepository.add(user);
