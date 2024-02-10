3b27bc01766fff3a3790e12c9e62fbc08bd007aa
Admin <funedy@yandex.ru>
2024-02-10 17:46:15 +0300

    add servlet

---
+++ src/main/java/by/teachmeskills/controller/UserServlet.java
@@ -0,0 +1,19 @@
+package by.teachmeskills.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class UserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write("Hello!");
+        printWriter.close();
+    }
+}
