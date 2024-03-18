c383f91049f27e4a1cc5ec1a8bae81088434b6af
Terabu <finediebun@gmail.com>
2024-03-18 12:47:58 +0300

    add timer cleaner

--- src/main/java/by/teachmeskills/repository/BucketInterfaceRepository.java
+++ src/main/java/by/teachmeskills/repository/BucketInterfaceRepository.java
@@ -2,7 +2,10 @@ package by.teachmeskills.repository;
 
 import by.teachmeskills.entity.Bucket;
 
+import java.util.List;
+
 public interface BucketInterfaceRepository {
     public Bucket add(Long orderId, Long productId, Long count);
     public void makeOrder(Long userId);
+    List getBucketsByOrderId(List orderIds);
 }
--- src/main/java/by/teachmeskills/repository/impl/BucketJdbcRepository.java
+++ src/main/java/by/teachmeskills/repository/impl/BucketJdbcRepository.java
@@ -9,14 +9,18 @@ import lombok.SneakyThrows;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class BucketJdbcRepository implements BucketInterfaceRepository {
     JDBCConnection connection = new PostgreSQL();
+    Connection connect = connection.getConnect();
+
 
     @SneakyThrows
     @Override
     public Bucket add(Long orderId, Long productId, Long count) {
-        Connection connect = connection.getConnect();
         PreparedStatement preparedStatementMax = connect.prepareStatement("select max(id) from apps.bucket");
         ResultSet resultSet = preparedStatementMax.executeQuery();
         resultSet.next();
@@ -47,6 +51,9 @@ public class BucketJdbcRepository implements BucketInterfaceRepository {
         bucket.setOrderId(orderId);
         bucket.setProductId(productId);
         bucket.setCount(count);
+
+        timerDeleteOrdersByBucket(orderId, count, productId);
+
         return bucket;
 
     }
@@ -54,10 +61,59 @@ public class BucketJdbcRepository implements BucketInterfaceRepository {
     @SneakyThrows
     @Override
     public void makeOrder(Long userId) {
-        Connection connect = connection.getConnect();
         PreparedStatement preparedStatement = connect.prepareStatement("UPDATE apps.orders SET status = ? where userid = ?");
         preparedStatement.setString(1, "Оформлен");
         preparedStatement.setLong(2, userId);
         preparedStatement.executeUpdate();
     }
+
+    @Override
+    public List getBucketsByOrderId(List orderIds) {
+        return null;
+    }
+    @SneakyThrows
+    public void timerDeleteOrdersByBucket(Long orderId,Long count, Long productId){
+        final String tableDeleteBucket = "DELETE FROM apps.bucket where orderid = ?";
+        final String tableDeleteOrders = "DELETE FROM apps.orders where id = ?";
+        final String selectCountProduct = "UPDATE apps.products SET quantity = ? where id = ?";
+
+        PreparedStatement preparedStatementStatus = connect.prepareStatement("select status from apps.orders WHERE id = ?");
+        preparedStatementStatus.setLong(1, orderId);
+        ResultSet status = preparedStatementStatus.executeQuery();
+        status.next();
+        String statusString = status.getString(1);
+
+        PreparedStatement quantity = connect.prepareStatement("SELECT quantity from apps.products WHERE id = ?");
+        quantity.setLong(1, productId);
+        ResultSet executeQuery = quantity.executeQuery();
+        executeQuery.next();
+        long quantityLong = executeQuery.getLong(1);
+
+        PreparedStatement preparedStatementProduct = connect.prepareStatement(selectCountProduct);
+        preparedStatementProduct.setLong(1, quantityLong + count);
+        preparedStatementProduct.setLong(2, productId);
+
+
+
+        PreparedStatement preparedStatementBucket = connect.prepareStatement(tableDeleteBucket);
+        preparedStatementBucket.setLong(1, orderId);
+        PreparedStatement preparedStatementOrders = connect.prepareStatement(tableDeleteOrders);
+        preparedStatementOrders.setLong(1, orderId);
+
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+
+            @SneakyThrows
+            public void run() {
+                if(statusString.equals("Создан") && statusString != null){
+                    preparedStatementProduct.executeUpdate();
+                    preparedStatementBucket.executeUpdate();
+                    preparedStatementOrders.executeUpdate();
+                }
+            }
+        };
+        long time = 7200000L;
+        timer.schedule(task, time);
+    }
+
 }
